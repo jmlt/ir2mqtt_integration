597cd04ff8ae588a638b5521e5e1ddb4bb68a1e8
Jorge <jtripeca@gmail.com>
2025-10-20 15:49:17 +0100

    1.3.4

--- custom_components/ir2mqtt/const.py
+++ custom_components/ir2mqtt/const.py
@@ -3,10 +3,11 @@ DOMAIN = "ir2mqtt"
 DEFAULT_TOPIC_PREFIX = "iracing"
 
 DEVICE_INFO = {
+    "identifiers": {(DOMAIN, "iracing_telemetry")},
     "name": "iRacing Telemetry",
     "manufacturer": "iR2mqtt",
     "model": "iR2mqtt Data Feed",
-    "sw_version": "1.3.3",
+    "sw_version": "1.3.4",
 }
 
 SENSOR_DEFINITIONS = {
@@ -46,6 +47,7 @@ SENSOR_DEFINITIONS = {
         "name": "Fuel Level",
         "value_template": "{{ value_json.fuel_level | float(0) | round(2) }}",
         "unit_of_measurement": "L",
+        "device_class": "volume",
         "icon": "mdi:gas-station",
     },
     "lap": {
@@ -82,12 +84,7 @@ SENSOR_DEFINITIONS = {
         "name": "Active Flag",
         "value_template": "{{ value_json.active_flags[0] if value_json.active_flags else 'No Flag' }}",
         "icon": "mdi:flag",
-        "json_attributes_template": "{ \"all_flags\": {{ value_json.active_flags | tojson }}, \"session_flags\": {{ value_json.session_flags }} }",
-    },
-    "timestamp": {
-        "name": "Timestamp",
-        "value_template": "{{ value_json.timestamp }}",
-        "icon": "mdi:clock-digital",
+        "json_attributes_template": '{ "all_flags": {{ value_json.active_flags | tojson }}, "session_flags": {{ value_json.session_flags }} }',
     },
     "incidents": {
         "name": "Incidents",
@@ -106,108 +103,148 @@ BINARY_SENSOR_DEFINITIONS = {
     "on_pit_road": {
         "name": "On Pit Road",
         "value_template": "{{ value_json.get('on_pit_road', False) }}",
-        "icon": "mdi:gauge",
+        "payload_on": "True",
+        "payload_off": "False",
+        "icon": "mdi:tire",
     },
-    
     "flag_checkered": {
         "name": "Checkered Flag",
         "value_template": "{{ 'Checkered' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag-checkered",
     },
     "flag_white": {
         "name": "White Flag",
         "value_template": "{{ 'White' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag-outline",
     },
     "flag_green": {
         "name": "Green Flag",
         "value_template": "{{ 'Green' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag-variant",
     },
     "flag_yellow": {
         "name": "Yellow Flag",
         "value_template": "{{ 'Yellow' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_red": {
         "name": "Red Flag",
         "value_template": "{{ 'Red' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_blue": {
         "name": "Blue Flag",
         "value_template": "{{ 'Blue' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_debris": {
         "name": "Debris Flag",
         "value_template": "{{ 'Debris' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_crossed": {
         "name": "Crossed Flag",
         "value_template": "{{ 'Crossed' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_yellow_waving": {
         "name": "Yellow Waving Flag",
         "value_template": "{{ 'Yellow Waving' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_black": {
         "name": "Black Flag",
         "value_template": "{{ 'Black' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_repair": {
         "name": "Repair Flag",
         "value_template": "{{ 'Repair' in value_json.get('active_flags', []) }}",
-        "icon": "mdi:flag",
+        "payload_on": "True",
+        "payload_off": "False",
+        "icon": "mdi:tools",
     },
     "flag_caution": {
         "name": "Caution Flag",
         "value_template": "{{ 'Caution' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_caution_waving": {
         "name": "Caution Waving Flag",
         "value_template": "{{ 'Caution Waving' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_disqualify": {
         "name": "Disqualify Flag",
         "value_template": "{{ 'Disqualify' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_furled": {
         "name": "Furled Flag",
         "value_template": "{{ 'Furled' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_green_held": {
         "name": "Green Held Flag",
         "value_template": "{{ 'Green Held' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
     "flag_random_waving": {
         "name": "Random Waving Flag",
         "value_template": "{{ 'Random Waving' in value_json.get('active_flags', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:flag",
     },
-
     "light_ready": {
         "name": "Ready Light",
         "value_template": "{{ 'Ready' in value_json.get('active_lights', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:traffic-light",
     },
     "light_set": {
         "name": "Set Light",
         "value_template": "{{ 'Set' in value_json.get('active_lights', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:traffic-light",
     },
     "light_go": {
         "name": "Go Light",
         "value_template": "{{ 'Go' in value_json.get('active_lights', []) }}",
+        "payload_on": "True",
+        "payload_off": "False",
         "icon": "mdi:traffic-light",
     },
 }
